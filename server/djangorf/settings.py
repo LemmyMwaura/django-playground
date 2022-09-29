a26aa162480f2137957afa56fbc96d404660bcab
lemmy mwaura <lemminton@gmail.com>
2022-09-29 03:57:29 -0700

    update settings (include apps)

--- server/djangorf/settings.py
+++ server/djangorf/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api.apps.ApiConfig',
+    'products.apps.ProductsConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
