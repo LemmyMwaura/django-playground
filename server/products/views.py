0f3e23f9de0374e5186d78f9114de9d27caa698d
lemmy mwaura <lemminton@gmail.com>
2022-09-30 04:49:22 -0700

    feat: setup products api routes

--- server/products/views.py
+++ server/products/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
-
+from rest_framework import generics
+from products.models import Product
+from products.serializers import ProductSerializer
 # Create your views here.
+
+class ProductCreateAPIView(generics.CreateAPIView):
+  queryset = Product.objects.all()
+  serializer_class = ProductSerializer
+
+product_create_view = ProductCreateAPIView.as_view()
+
+class ProductDetailAPIView(generics.RetrieveAPIView):
+  queryset = Product.objects.all()
+  serializer_class = ProductSerializer
+
+product_detail_view = ProductDetailAPIView.as_view()
