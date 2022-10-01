b01ddac83b209472788daa1065536c164d3deb3d
lemmy mwaura <lemminton@gmail.com>
2022-10-01 03:13:09 -0700

    feat: update views

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,43 +1,12 @@
-import { useEffect, useState } from "react"
 import "./App.css"
-
-// axiosInstance
-import axiosInstance from "./utils/axios"
-
-interface ServerResponse {
-  data: ServerData
-}
-
-interface ServerData {
-  title: string
-  content: string
-  price : number
-}
+import Post from "./components/Post"
+import { CreatePost } from './components/Post'
 
 function App() {
-  const [data, setData] = useState(null)
-
-  useEffect(() => {
-    const getData = async () => {
-      const { data } = await axiosInstance
-        .get<ServerData>("/api")
-        .catch((e) => console.log(e))
-
-      setData(data)
-    }
-
-    getData()
-  }, [])
-
   return (
     <div className="App">
-      <h2>Home</h2>
-      <div>
-        <h2>DATA</h2>
-        <h3>{data?.title}</h3>
-        <h3>{data?.content}</h3>
-        <h3>{data?.price}</h3>
-      </div>
+      <Post />
+      <CreatePost />
     </div>
   )
 }
--- server/products/views.py
+++ server/products/views.py
@@ -1,16 +1,84 @@
 from rest_framework import generics
 from products.models import Product
 from products.serializers import ProductSerializer
+
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from django.shortcuts import get_object_or_404
 # Create your views here.
 
-class ProductCreateAPIView(generics.CreateAPIView):
+class ProductListCreateAPIView(generics.ListCreateAPIView):
   queryset = Product.objects.all()
   serializer_class = ProductSerializer
 
-product_create_view = ProductCreateAPIView.as_view()
+  def perform_create(self, serializer):
+    title = serializer.validated_data.get('title') 
+    content = serializer.validated_data.get('content') or None
+    if content is None:
+      content = title
+    serializer.save(content=content)
+    # send a signal
+    # return super().perform_create(serializer)
+
+product_list_create_view = ProductListCreateAPIView.as_view()
 
 class ProductDetailAPIView(generics.RetrieveAPIView):
+  # get from id
   queryset = Product.objects.all()
   serializer_class = ProductSerializer
 
 product_detail_view = ProductDetailAPIView.as_view()
+
+class ProductListAPIView(generics.ListAPIView):
+  queryset = Product.objects.all()
+  serializer_class = ProductSerializer
+
+product_list_view = ProductListAPIView.as_view()
+
+class ProductUpdateAPIView(generics.UpdateAPIView):
+  queryset = Product.objects.all()
+  serializer_class = ProductSerializer
+  lookup_field = "pk"
+
+  def perform_update(self, serializer):
+    instance = serializer.save()
+    if not instance.content:
+      instance.content = instance.title
+
+product_update_view = ProductUpdateAPIView.as_view()
+
+class ProductDeleteAPIView(generics.DestroyAPIView):
+  queryset = Product.objects.all()
+  serializer_class = ProductSerializer
+
+product_delete_view = ProductDeleteAPIView.as_view()
+
+@api_view(['GET','POST'])
+def product_alt_view(request, pk=None, *args, **kwargs):
+  # list, create and detail view
+  method = request.method
+
+  if method == "GET":
+    if pk is not None:
+      obj = get_object_or_404(Product, pk=pk)
+      data = ProductSerializer(obj).data
+    else:
+      queryset = Product.objects.all(pk=pk)
+      data = ProductSerializer(queryset, many=True).data
+    return Response(data)
+
+  if method == "POST":
+    #create an item
+    serializer = ProductSerializer(data=request.data)
+
+    if serializer.is_valid(raise_exception=True):
+      title = serializer.validated_data.get('title') 
+      content = serializer.validated_data.get('content') or None
+
+      if content is None:
+        content = title
+
+      serializer.save(content=content)
+      return Response(serializer.data)
+    return Response({"invalid":"not good data"}, status=400)
