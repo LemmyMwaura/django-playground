7ce2da6312298da9733adac87efaee0ee3828743
lemmy mwaura <lemminton@gmail.com>
2022-10-03 04:47:16 -0700

    feat: add obtain-auth-token route

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,6 +1,8 @@
+from rest_framework.authtoken.views import obtain_auth_token
 from django.urls import path
 from . import views
 
 urlpatterns = [
+  path('auth/', obtain_auth_token),
   path('', views.api_home)
 ]
