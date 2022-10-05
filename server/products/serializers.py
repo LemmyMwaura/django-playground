8696ded2112dcb9a6450b9258211dd3b7ff91b84
lemmy mwaura <lemminton@gmail.com>
2022-10-05 04:17:00 -0700

    feat: add fetch related products functionality(method field)

--- server/products/serializers.py
+++ server/products/serializers.py
@@ -2,15 +2,20 @@ from rest_framework import serializers
 from rest_framework.reverse import reverse
 from .models import Product
 from .validators import validate_title_no_hello, unique_title_validator
+from api.serializers import UserPublicSerializer, UserProductInlineSerializer
 
 class ProductSerializer(serializers.ModelSerializer):
+  owner = UserPublicSerializer(source='user', read_only=True)
   url = serializers.HyperlinkedIdentityField(view_name='product-detail', lookup_field='pk')
   edit_url = serializers.SerializerMethodField(read_only=True)
   email = serializers.EmailField(write_only=True)
   title = serializers.CharField(validators=[validate_title_no_hello, unique_title_validator])
+  related_products = serializers.SerializerMethodField(read_only=True)
+
   class Meta:
     model = Product
     fields = [
+      'owner',
       'pk',
       'url', 
       'email',
@@ -19,7 +24,8 @@ class ProductSerializer(serializers.ModelSerializer):
       'content', 
       'price', 
       'sale_price', 
-      'get_discount'
+      'get_discount',
+      'related_products'
     ]
 
   def create(self, validated_data):
@@ -29,3 +35,8 @@ class ProductSerializer(serializers.ModelSerializer):
   def get_edit_url(self, obj):
     request = self.context.get('request')
     return reverse('product-edit', request=request, kwargs={'pk': obj.pk}) if request is not None else None
+
+  def get_related_products(self, obj):
+    user = obj.user
+    my_products_qs = user.product_set.all().order_by("?")[:3]
+    return UserProductInlineSerializer(my_products_qs, context=self.context, many=True).data
