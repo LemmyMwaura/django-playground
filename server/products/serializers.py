7569f39ee61433aa71fc9d5fcdf488aa992718c8
lemmy mwaura <lemminton@gmail.com>
2022-10-05 00:51:37 -0700

    feat: add validators

--- server/products/serializers.py
+++ server/products/serializers.py
@@ -1,8 +1,27 @@
 from rest_framework import serializers
+from rest_framework.reverse import reverse
 from .models import Product
+from .validators import validate_title_no_hello, unique_title_validator
 
 class ProductSerializer(serializers.ModelSerializer):
+  url = serializers.HyperlinkedIdentityField(view_name='product-detail', lookup_field='pk')
+  edit_url = serializers.SerializerMethodField(read_only=True)
+  email = serializers.EmailField(write_only=True)
+  title = serializers.CharField(validators=[validate_title_no_hello, unique_title_validator])
   class Meta:
     model = Product
-    fields = ['title', 'content', 'price', 'sale_price', 'get_discount']
+    fields = [
+      'pk',
+      'url', 
+      'email',
+      'edit_url',
+      'title', 
+      'content', 
+      'price', 
+      'sale_price', 
+      'get_discount'
+    ]
 
+  def get_edit_url(self, obj):
+    request = self.context.get('request')
+    return reverse('product-edit', request=request, kwargs={'pk': obj.pk}) if request is not None else None
---
+++ server/products/validators.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+from .models import Product
+
+def validate_title_no_hello(value):
+  if "hello" in value.lower():
+    raise serializers.ValidationError(f'{value} is not allowed')
+
+unique_title_validator = UniqueValidator(queryset=Product.objects.all(), lookup='iexact')
